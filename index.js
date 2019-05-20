a2f377d1a9f2921d897208d95a12911447a8cbb1
frkozbk <ozbekfaruk12@gmail.com>
2019-05-20 15:46:59 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+// Import Routes
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+  console.log("connected to db!");
+});
+//Middleware
+app.use(express.json());
+
+//Route Middleware
+app.use("/api/user", authRoute);
+app.use("/api/post", postRoute);
+
+app.listen(3000, () => console.log("Server Up and running"));
